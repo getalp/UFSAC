21e902169dadfaebef75bc30fa05982b383f2629
Loïc Vial <loic.vial@imag.fr>
2018-04-03 16:30:50 +0200

    Clean script

--- java/src/main/java/ConvertOriginalCorpora.java
+++ java/src/main/java/ConvertOriginalCorpora.java
@@ -12,7 +12,23 @@ public class ConvertOriginalCorpora
     public static void main(String[] args) throws Exception
     {
         OriginalCorpusConverter converter = new OriginalCorpusConverter();
-/*
+        
+        converter.convert(new Senseval1Converter(), "data/original_corpus/senseval1", "data/corpus/senseval1.xml", 16, 30, false);
+
+        converter.convert(new Senseval2Converter(), "data/original_corpus/mihalcea/senseval2", "data/corpus/senseval2.xml", 171, 30, false);
+
+        converter.convert(new Senseval3Task1Converter(), "data/original_corpus/mihalcea/senseval3", "data/corpus/senseval3task1.xml", 171, 30, false);
+
+        converter.convert(new Senseval3Task6Converter(), "data/original_corpus/senseval3task6", "data/corpus/senseval3task6.xml", 171, 30, false);
+        
+        converter.convert(new Semeval2007Task7Converter(), "data/original_corpus/semeval/2007/task7", "data/corpus/semeval2007task7.xml", 21, 30, false);
+        
+        converter.convert(new Semeval2007Task17Converter(), "data/original_corpus/semeval/2007/task17", "data/corpus/semeval2007task17.xml", 21, 30, false);
+        
+        converter.convert(new Semeval2013Task12Converter(), "data/original_corpus/semeval/2013/task12", "data/corpus/semeval2013task12.xml", 30, 30, false);
+        
+        converter.convert(new Semeval2015Task13Converter(), "data/original_corpus/semeval/2015/task13", "data/corpus/semeval2015task13.xml", 30, 30, false);
+
         converter.convert(new RaganatoUnifiedConverter(), "data/original_corpus/raganato/senseval2/senseval2", "data/corpus/raganato_senseval2.xml", 30, 30, false);
         
         converter.convert(new RaganatoUnifiedConverter(), "data/original_corpus/raganato/senseval3/senseval3", "data/corpus/raganato_senseval3.xml", 30, 30, false);
@@ -36,20 +52,6 @@ public class ConvertOriginalCorpora
         converter.convert(new OMSTIConverter(), "data/original_corpus/omsti/30", "data/corpus/omsti.xml", 30, 30, true);
 
         converter.convert(new OntonotesConverter(), "data/original_corpus/ontonotes/5.0/data/files/data/english", "data/corpus/ontonotes.xml", 30, 30, true);
-        
-        converter.convert(new Senseval1Converter(), "data/original_corpus/senseval1", "data/corpus/senseval1.xml", 16, 30, false);
-*/      
-        converter.convert(new Senseval2Converter(), "data/original_corpus/mihalcea/senseval2", "data/corpus/senseval2.xml", 171, 30, false);
-
-        converter.convert(new Senseval3Task1Converter(), "data/original_corpus/mihalcea/senseval3", "data/corpus/senseval3task1.xml", 171, 30, false);
-        
-        converter.convert(new Semeval2007Task7Converter(), "data/original_corpus/semeval/2007/task7", "data/corpus/semeval2007task7.xml", 21, 30, false);
-        
-        converter.convert(new Semeval2007Task17Converter(), "data/original_corpus/semeval/2007/task17", "data/corpus/semeval2007task17.xml", 21, 30, false);
-        
-        converter.convert(new Semeval2013Task12Converter(), "data/original_corpus/semeval/2013/task12", "data/corpus/semeval2013task12.xml", 30, 30, false);
-        
-        converter.convert(new Semeval2015Task13Converter(), "data/original_corpus/semeval/2015/task13", "data/corpus/semeval2015task13.xml", 30, 30, false);
 
     }
 }
