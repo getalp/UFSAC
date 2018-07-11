4f0d0ec4685bb9a769ff0bd63de962f3eae78a86
Loïc Vial <contact.coyl@gmail.com>
2018-07-11 19:29:30 +0200

    Add SensEval 3 task 6 (lexical sample) converter

---
+++ java/src/main/java/getalp/wsd/common/utils/SenseKeyUtils.java
@@ -0,0 +1,16 @@
+package getalp.wsd.common.utils;
+
+public class SenseKeyUtils 
+{
+	public static String extractLemmaFromSenseKey(String senseKey)
+	{
+		return senseKey.substring(0, senseKey.indexOf("%"));
+	}
+	
+	public static String extractPOSFromSenseKey(String senseKey)
+	{
+		int index = senseKey.indexOf("%");
+		String posAsInt = senseKey.substring(index + 1, index + 2);
+		return POSConverter.toWNPOS(Integer.valueOf(posAsInt));
+	}
+}
--- java/src/main/java/getalp/wsd/ufsac/converter/Senseval3Task6Converter.java
+++ java/src/main/java/getalp/wsd/ufsac/converter/Senseval3Task6Converter.java
@@ -1,54 +1,51 @@
 package getalp.wsd.ufsac.converter;
 
 import org.xml.sax.*;
-import org.xml.sax.helpers.XMLReaderFactory;
-import getalp.wsd.ufsac.core.Corpus;
-import getalp.wsd.ufsac.core.Document;
-import getalp.wsd.ufsac.core.Paragraph;
+
+import getalp.wsd.common.utils.File;
+import getalp.wsd.common.utils.RegExp;
+import getalp.wsd.common.utils.SenseKeyUtils;
+import getalp.wsd.common.utils.StringUtils;
+import getalp.wsd.common.xml.SAXBasicHandler;
 import getalp.wsd.ufsac.core.Sentence;
 import getalp.wsd.ufsac.core.Word;
-import org.xml.sax.helpers.DefaultHandler;
+import getalp.wsd.ufsac.streaming.writer.StreamingCorpusWriterSentence;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Senseval3Task6Converter extends DefaultHandler implements UFSACConverter
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class Senseval3Task6Converter implements UFSACConverter
 {
-    private Corpus currentCorpus;
+    private Map<String, List<String>> verbKeyToWordnetKey;
 
-    private Document currentDocument;
-
-    private Paragraph currentParagraph;
-
-    private Sentence currentSentence;
-
-    private boolean saveCharacters;
-
-    private String currentCharacters;
-
-    private String currentPos;
-
-    private String currentLemma;
-
-    private String currentContextKey;
-
-    private Map<String, String> currentIdToContextKey;
-
-    private Map<String, String> sensesById;
+    private Map<String, List<String>> sensesById;
     
-    //private int wnVersion;
-
     @Override
     public void convert(String inpath, String outpath, int wnVersion)
     {
         try
         {
-            //this.wnVersion = wnVersion;
+        	verbKeyToWordnetKey = new HashMap<>();
             sensesById = new HashMap<>();
-            loadKeys(inpath + "/test/EnglishLS.test.key");
-            loadMappingForVerbs(inpath + "/test/EnglishLS.dictionary.mapping.xml");
-            loadCorpus(inpath + "/test/EnglishLS.test");
+            loadMappingForVerbs(inpath + "/EnglishLS.dictionary.mapping.xml");
+            loadKeys(inpath + "/EnglishLS.test.key");
+            loadCorpus(inpath + "/EnglishLS.test", outpath);
         } 
         catch (Exception e)
         {
@@ -56,143 +53,213 @@ public class Senseval3Task6Converter extends DefaultHandler implements UFSACConv
         }
     }
 
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException
-    {
-        if (localName.equals("instance"))
-        {
-            String currentId = atts.getValue("id");
-            currentContextKey = currentIdToContextKey.get(currentId);
-            currentLemma = currentContextKey.substring(0, currentContextKey.indexOf("."));
-            currentSentence = new Sentence(currentParagraph);
-            saveCharacters = true;
-            currentCharacters = "";
-        } 
-        else if (localName.equals("head"))
-        {
-            String[] wordsBefore = currentCharacters.split("\\s+");
-            int indexOfLastDot = -1;
-            for (int i = 0; i < wordsBefore.length; i++)
+    public void loadMappingForVerbs(String path) throws Exception
+    {       
+    	BufferedReader reader = Files.newBufferedReader(Paths.get(path));
+    	String tmpfilepath = File.createTemporaryFileName();
+    	BufferedWriter writer = Files.newBufferedWriter(Paths.get(tmpfilepath));
+    	String line;
+    	while ((line = reader.readLine()) != null)
+    	{
+    		if (line.contains("<sense"))
+    		{
+    			line = line.substring(0, line.indexOf("synset=\""));
+    			line += "/>";
+    		}
+    		writer.write(line + "\n");
+    	}
+    	writer.close();
+        SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+        SAXParser parser = saxFactory.newSAXParser();
+        parser.parse(
+                new SequenceInputStream(
+                    Collections.enumeration(Arrays.asList(
+                    new InputStream[] {
+                        new ByteArrayInputStream("<dummy>".getBytes()),
+                        new FileInputStream(tmpfilepath),
+                        new ByteArrayInputStream("</dummy>".getBytes()),
+                    }))
+                ), 
+    	new SAXBasicHandler()
+		{
+        	private boolean inVerb = false;
+        	
+            @Override
+            public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException
             {
-                if (wordsBefore[i].equals("."))
-                {
-                    indexOfLastDot = i;
-                }
+            	if (qName.equals("lexelt"))
+            	{
+            		if (atts.getValue("item").endsWith(".v"))
+            		{
+            			inVerb = true;
+            		}
+            		else
+            		{
+            			inVerb = false;
+            		}
+            	}
+            	else if (qName.equals("sense"))
+            	{
+            		if (inVerb && atts.getValue("wn") != null)
+            		{
+            			verbKeyToWordnetKey.put(atts.getValue("id"), Arrays.asList(atts.getValue("wn").split(";")));
+            		}
+            	}
             }
-            for (int i = indexOfLastDot + 1; i < wordsBefore.length; i++)
-            {
-                if (!wordsBefore[i].isEmpty())
-                {
-                    Word w = new Word(currentSentence);
-                    w.setValue(wordsBefore[i]);
-                }
-            }
-            currentCharacters = "";
-        }
-    }
-
-    @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException
-    {
-        if (localName.equals("instance"))
-        {
-            String[] wordsAfter = currentCharacters.split("\\s+");
-            int indexOfFirstDot = wordsAfter.length;
-            for (int i = wordsAfter.length - 1; i >= 0; i--)
-            {
-                if (wordsAfter[i].equals("."))
-                {
-                    indexOfFirstDot = i;
-                }
-            }
-            for (int i = 0; i < indexOfFirstDot; i++)
-            {
-                if (!wordsAfter[i].isEmpty())
-                {
-                    Word w = new Word(currentSentence);
-                    w.setValue(wordsAfter[i]);
-                }
-                Word w = new Word(currentSentence);
-                w.setValue(".");
-                currentCharacters = "";
-                saveCharacters = false;
-
-            }
-        } 
-        else if (localName.equals("head"))
-        {
-            Word w = new Word(currentSentence);
-            w.setValue(currentCharacters.trim());
-            w.setAnnotation("lemma", currentLemma);
-            w.setAnnotation("pos", currentPos);
-            currentCharacters = "";
-        }
-    }
-
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException
-    {
-        if (saveCharacters)
-        {
-            currentCharacters += new String(ch, start, length);
-        }
+		});
+        File.removeFile(tmpfilepath);
     }
 
     public void loadKeys(String path) throws Exception
     {
-        currentIdToContextKey = new HashMap<>();
         BufferedReader br = new BufferedReader(new FileReader(path));
         String line;
         while ((line = br.readLine()) != null)
         {
-            String[] tokens = line.split("\\s+");
-            if (tokens[0].contains(".v"))
-            {
-                currentIdToContextKey.put(tokens[2], tokens[1]);
-            } 
-            else if (tokens[0].contains(".n") || tokens[0].contains(".a"))
-            { 
-                String currentSense = tokens[1];
-                sensesById.put(tokens[2], currentSense); 
-            }
+            String[] tokens = line.split(RegExp.anyWhiteSpaceGrouped.pattern());
+            String tokenId = tokens[1];
+            List<String> senses = new ArrayList<>();
+        	for (int i = 2 ; i < tokens.length ; i++)
+        	{
+        		if (!tokens[i].equals("U"))
+        		{
+                    if (tokens[0].endsWith(".v"))
+                    {
+                    	if (verbKeyToWordnetKey.containsKey(tokens[i]))
+                    	{
+                    		senses.addAll(verbKeyToWordnetKey.get(tokens[i]));
+                    	}
+                    }
+                    else
+                    {
+                    	senses.add(tokens[i]);
+                    }
+        		}
+        	}
+        	sensesById.put(tokenId, senses);
         }
         br.close();
     }
 
-    public void loadMappingForVerbs(String path) throws Exception
+    private void loadCorpus(String inpath, String outpath) throws Exception
     {
-        BufferedReader br = new BufferedReader(new FileReader(path));
-        String line;
-        while ((line = br.readLine()) != null)
-        {
-            if (line.contains("sense"))
-            {
-                String[] tokens = line.split("\\s+");
-                if (tokens[3].contains("wn"))
-                {
-                    String[] currentSenses = tokens[3].split("\"");
-                    String currentSense = currentSenses[1];
+    	BufferedReader reader = Files.newBufferedReader(Paths.get(inpath));
+    	String tmpfilepath = File.createTemporaryFileName();
+    	BufferedWriter writer = Files.newBufferedWriter(Paths.get(tmpfilepath));
+    	String line;
+    	while ((line = reader.readLine()) != null)
+    	{
+    		if (line.contains("&frac12 ;"))
+    		{
+    			line = line.replaceAll("&frac12 ;", "1/2");
+    		}
+    		if (line.contains("&frac14 ;"))
+    		{
+    			line = line.replaceAll("&frac14 ;", "1/4");
+    		}
+    		if (line.contains("&frac34 ;"))
+    		{
+    			line = line.replaceAll("&frac34 ;", "3/4");
+    		}
+    		if (line.contains("&deg/60 ;"))
+    		{
+    			line = line.replaceAll("&deg/60 ;", "deg");
+    		}
+    		writer.write(line + "\n");
+    	}
+    	writer.close();
+    	
+    	StreamingCorpusWriterSentence out = new StreamingCorpusWriterSentence();
+        SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+        SAXParser parser = saxFactory.newSAXParser();
 
-                    String[] currentIds = tokens[1].split("\"");
-                    String currentId = currentIds[1];
-                    
-                    sensesById.put(currentIdToContextKey.get(currentId), currentSense);
+        out.open(outpath);
+        
+        parser.parse(
+            new SequenceInputStream(
+                Collections.enumeration(Arrays.asList(
+                new InputStream[] {
+                    new ByteArrayInputStream("<dummy>".getBytes()),
+                    new FileInputStream(tmpfilepath),
+                    new ByteArrayInputStream("</dummy>".getBytes()),
+                }))
+            ), 
+        new SAXBasicHandler()
+		{
+        	private String currentTokenId = "";
+        	
+        	private Sentence currentSentence = new Sentence();
+        	
+            @Override
+            public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException
+            {
+                if (qName.equals("instance"))
+                {
+                	currentTokenId = atts.getValue("id");
+                	currentSentence = new Sentence();
+                	resetAndStartSaveCharacters();
+                }
+                else if (qName.equals("head"))
+                {
+                    String[] wordsBefore = getAndStopSaveCharacters().split(RegExp.anyWhiteSpaceGrouped.pattern());
+                    int indexOfLastDot = -1;
+                    for (int i = wordsBefore.length - 1; i >= 0; i--)
+                    {
+                        if (wordsBefore[i].equals("."))
+                        {
+                            indexOfLastDot = i;
+                            break;
+                        }
+                    }
+                    for (int i = indexOfLastDot + 1; i < wordsBefore.length; i++)
+                    {
+                        Word w = new Word(currentSentence);
+                        w.setValue(wordsBefore[i]);
+                    }
+                    resetAndStartSaveCharacters();
                 }
             }
 
-        }
-        br.close();
+            @Override
+            public void endElement(String uri, String localName, String qName) throws SAXException
+            {
+                if (qName.equals("instance"))
+                {
+                    String[] wordsAfter = getAndStopSaveCharacters().split(RegExp.anyWhiteSpaceGrouped.pattern());
+                    int indexOfFirstDot = wordsAfter.length;
+                    for (int i = 0; i < wordsAfter.length; i++)
+                    {
+                        if (wordsAfter[i].equals("."))
+                        {
+                            indexOfFirstDot = i;
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < indexOfFirstDot; i++)
+                    {
+                        Word w = new Word(currentSentence);
+                        w.setValue(wordsAfter[i]);
+                    }
+                    Word w = new Word(currentSentence);
+                    w.setValue(".");
+                    out.writeSentence(currentSentence);
+                } 
+                else if (qName.equals("head"))
+                {
+                    Word w = new Word(currentSentence);
+                    w.setValue(getAndStopSaveCharacters());
+                    List<String> senseKeys = sensesById.get(currentTokenId);
+                    if (senseKeys != null && !senseKeys.isEmpty())
+                    {
+	                    w.setAnnotation("lemma", SenseKeyUtils.extractLemmaFromSenseKey(senseKeys.get(0)));
+	                    w.setAnnotation("pos", SenseKeyUtils.extractPOSFromSenseKey(senseKeys.get(0)));
+	                    w.setAnnotation("wn171_key", StringUtils.join(senseKeys, ";"));
+                    }
+                    resetAndStartSaveCharacters();
+                }
+            }
+		});
+        out.close();
+        File.removeFile(tmpfilepath);
     }
-
-    private Corpus loadCorpus(String path) throws Exception
-    {
-        currentCorpus = new Corpus();
-        currentDocument = new Document(currentCorpus);
-        currentParagraph = new Paragraph(currentDocument);
-        XMLReader saxReader = XMLReaderFactory.createXMLReader();
-        saxReader.setContentHandler(this);
-        saxReader.parse(path);
-        return currentCorpus;
-    }
-
 }
